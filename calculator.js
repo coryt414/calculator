081d338ca844dbc66902d2b2b12d6cb34a0e7d4e
Cory Thomas <coryt414@gmail.com>
2024-06-10 15:38:24 -0400

    Complete math functions

--- calculator.js
+++ calculator.js
@@ -18,11 +18,47 @@ function divide(inputNumber1, inputNumber2) {
 };
 
 
+const buttons = document.querySelectorAll("button");
+let displayInput1 = document.querySelector(".display-input-1");
+let displayOperator = document.querySelector(".display-input-operator");
+let displayInput2 = document.querySelector(".display-input-2");
+let displayOutput = document.querySelector(".display-output");
+let expression = [];
 
-
-let inputNumber1 = ''
-let inputNumber2 = ''
-let operator = ''
+buttons.forEach((button) => {
+    button.addEventListener("click", () => {
+            if (button.parentElement.className === "numbers-container") {
+                if (displayOperator.textContent === '') {
+                    displayInput1.textContent += button.textContent;
+                }
+                else {
+                    displayInput2.textContent += button.textContent;
+                }
+            }
+            else if (button.parentElement.className === "operators-container") {
+                let operator = button.textContent;
+                let inputNumber1 = Number(displayInput1.textContent);
+                displayOperator.textContent = operator;
+                expression = [operator, inputNumber1]
+                return expression
+            }
+            else if (button.parentElement.className === "equals-container") {
+                let inputNumber2 = Number(displayInput2.textContent);
+                expression.push(inputNumber2);
+                console.log(expression)
+                console.log( operate(expression[0],expression[1],expression[2]))
+                displayOutput.textContent = operate(expression[0],expression[1],expression[2]);
+            }
+            else if (button.parentElement.className === 'clear-container') {
+                displayInput1.textContent = '';
+                displayOperator.textContent = '';
+                displayInput2.textContent = '';
+                inputNumber1 = '';
+                operator = '';
+                inputNumber2 = '';
+            }
+    })
+});
 
 function operate(operator, inputNumber1, inputNumber2) {
     switch (operator) {
@@ -32,7 +68,7 @@ function operate(operator, inputNumber1, inputNumber2) {
         case '-':
             return subtract(inputNumber1, inputNumber2);
             
-        case 'x':
+        case '*':
             return multiply(inputNumber1, inputNumber2);
             
         case '/':
