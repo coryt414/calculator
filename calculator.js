e6fd6dcb4841079f0ce6cdb8520c6236b8a863aa
Cory Thomas <coryt414@gmail.com>
2024-06-10 17:07:27 -0400

    Add chained expression feature

--- calculator.js
+++ calculator.js
@@ -23,40 +23,62 @@ let displayInput1 = document.querySelector(".display-input-1");
 let displayOperator = document.querySelector(".display-input-operator");
 let displayInput2 = document.querySelector(".display-input-2");
 let displayOutput = document.querySelector(".display-output");
+let inputNumber1 = '';
+let inputNumber2 = '';
+let operator = '';
 let expression = [];
 
 buttons.forEach((button) => {
     button.addEventListener("click", () => {
-            if (button.parentElement.className === "numbers-container") {
-                if (displayOperator.textContent === '') {
-                    displayInput1.textContent += button.textContent;
-                }
-                else {
-                    displayInput2.textContent += button.textContent;
-                }
+        if (button.parentElement.className === "numbers-container") {
+            displayOutput.textContent = '';
+            if (displayOperator.textContent === '') {
+                displayInput1.textContent += button.textContent;
             }
-            else if (button.parentElement.className === "operators-container") {
-                let operator = button.textContent;
-                let inputNumber1 = Number(displayInput1.textContent);
+            else {
+                displayInput2.textContent += button.textContent;
+            }
+        }
+        else if (button.parentElement.className === "operators-container") {
+            operator = button.textContent;
+            if (displayInput2.textContent !== ''){
+                inputNumber2 = Number(displayInput2.textContent)
+                expression.push(inputNumber2);
+                displayOperator.textContent = operator;
+                displayInput2.textContent = '';
+                inputNumber1 = operate(expression[0],expression[1],expression[2]);
+                expression = [operator, inputNumber1];
+                displayInput1.textContent = inputNumber1
+            } 
+                else {
+                inputNumber1 = Number(displayInput1.textContent);
                 displayOperator.textContent = operator;
                 expression = [operator, inputNumber1]
                 return expression
-            }
-            else if (button.parentElement.className === "equals-container") {
-                let inputNumber2 = Number(displayInput2.textContent);
-                expression.push(inputNumber2);
-                console.log(expression)
-                console.log( operate(expression[0],expression[1],expression[2]))
-                displayOutput.textContent = operate(expression[0],expression[1],expression[2]);
-            }
-            else if (button.parentElement.className === 'clear-container') {
-                displayInput1.textContent = '';
-                displayOperator.textContent = '';
-                displayInput2.textContent = '';
-                inputNumber1 = '';
-                operator = '';
-                inputNumber2 = '';
-            }
+                }
+        }
+        else if (button.parentElement.className === "equals-container") {
+            inputNumber2 = Number(displayInput2.textContent);
+            expression.push(inputNumber2);
+            console.log(expression)
+            console.log( operate(expression[0],expression[1],expression[2]))
+            displayOutput.textContent = operate(expression[0],expression[1],expression[2]);
+            displayInput1.textContent = '';
+            displayOperator.textContent = '';
+            displayInput2.textContent = '';
+            inputNumber1 = '';
+            operator = '';
+            inputNumber2 = '';
+        }
+        else if (button.parentElement.className === 'clear-container') {
+            displayInput1.textContent = '';
+            displayOperator.textContent = '';
+            displayInput2.textContent = '';
+            displayOutput.textContent = '';
+            inputNumber1 = '';
+            operator = '';
+            inputNumber2 = '';
+        }
     })
 });
 
